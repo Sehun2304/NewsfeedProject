d76b3cfb10bc14ee1bc1b92e7fe71ad6994c94d9
ohoh2302 <ssjj2302@gmail.com>
2023-07-05 17:09:21 +0900

    댓글CRUD 구현

---
+++ src/main/java/com/sparta/newsfeedproject/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.newsfeedproject.repository;
+
+import com.sparta.newsfeedproject.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
---
+++ src/main/java/com/sparta/newsfeedproject/service/CommentService.java
@@ -0,0 +1,50 @@
+package com.sparta.newsfeedproject.service;
+
+import com.sparta.newsfeedproject.dto.CommentRequestDto;
+import com.sparta.newsfeedproject.dto.CommentResponseDto;
+import com.sparta.newsfeedproject.entity.Comment;
+import com.sparta.newsfeedproject.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+    public CommentResponseDto createComment(CommentRequestDto commentRequestDto) {
+
+        Comment comment = new Comment(commentRequestDto);
+
+        Comment saveComment = commentRepository.save(comment);
+        return new CommentResponseDto(saveComment);
+    }
+
+    @Transactional
+    public CommentResponseDto updateComment(Long id, CommentRequestDto commentRequestDto) {
+        Comment comment = findComment(id);
+
+        comment.checkPassword(commentRequestDto.getPassword());
+
+        comment.setBody(commentRequestDto.getBody());
+
+        return new CommentResponseDto(comment);
+    }
+
+    public void deleteComment(Long id, String password) {
+        Comment comment = findComment(id);
+
+        comment.checkPassword(password);
+
+
+        commentRepository.delete(comment);
+    }
+    private Comment findComment(Long id) {
+        return CommentRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+    }
+
+}
