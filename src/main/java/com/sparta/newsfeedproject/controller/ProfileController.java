366c5e1a79e4da998fd50007880b8b9dcdb16403
sehun <ban6276@gmail.com>
2023-07-06 21:25:26 +0900

    profile 수정중

--- src/main/java/com/sparta/newsfeedproject/controller/ProfileController.java
+++ src/main/java/com/sparta/newsfeedproject/controller/ProfileController.java
@@ -16,29 +16,29 @@ import org.springframework.web.bind.annotation.*;
 
 public class ProfileController {
 
+    private final UserService userService;
+
 
     //프로필 가져오기
     @GetMapping("/{id}")
-    public UserResponseDto getUser(@PathVariable Long id){
-        return UserService.getUser(id);
+    public UserResponseDto viewUser(@PathVariable Long id){
+
+        return userService.viewUser(id);
     }
     //프로필 수정
     @PatchMapping("/{id}/intro")
     public ResponseEntity<ApiResponseDto> updateUser(@PathVariable Long id, @RequestBody UpdateRequestDto updateRequestDto){
-        return UserService.updateUser(id, updateRequestDto);
+        return userService.updateUser(id, updateRequestDto);
     }
 
     //비밀번호 수정
 
-//    @PatchMapping("/{id}/pass")
-//    public String updatePass(@PathVariable Long id,@RequestBody PassUpdateDto passUpdateDto){
-//        if(!id.equals(user.getUser().getId())) {
-//            return "회원정보 수정은 본인만 가능합니다.";
-//        }
-//        passService.pwUpdate(id,passUpdateDto);
-//        return "비밀번호 수정이 완료되었습니다.";
-//    }
-//    }
+    @PatchMapping("/{id}/pass")
+    public String updatePass(@PathVariable Long id,@RequestBody PassUpdateDto passUpdateDto){
+
+        return  null;
+    }
+    }
 
 
 
@@ -46,4 +46,4 @@ public class ProfileController {
 
 
 
-}
+
--- src/main/java/com/sparta/newsfeedproject/service/UserService.java
+++ src/main/java/com/sparta/newsfeedproject/service/UserService.java
@@ -9,6 +9,8 @@ import com.sparta.newsfeedproject.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     private static UserRepository userRepository;
@@ -31,17 +33,36 @@ public class UserService {
 
 
 
-    public static UserResponseDto getUser(Long id){
+    public UserResponseDto viewUser(Long id){
         User user = findUser(id);
         return new UserResponseDto(user);
     }
 
 
+
     public static ResponseEntity<ApiResponseDto> updateUser(Long id, UpdateRequestDto updateRequestDto) {
-        return null;
+    Optional<User> optionalUser = userRepository.findById(id);
+
+    if (!optionalUser.isPresent()) {
+        throw new IllegalArgumentException("해당 ID의 사용자를 찾을 수 없습니다.");
     }
 
-    private static User findUser(Long id){
+    User updateUser = optionalUser.get();
+
+    if (!updateRequestDto.getPassword().equals(updateRequestDto.getCheckPassword())) {
+        throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+    }
+
+    String newIntroduce = updateRequestDto.getNewIntroduce();
+    updateUser.setIntroduce(newIntroduce);
+
+    userRepository.save(updateUser);
+
+    return ResponseEntity.ok(new ApiResponseDto());
+}
+
+
+    private User findUser(Long id){
         return userRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("잘못된 유저입니다"));
     }
 
